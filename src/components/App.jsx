e6df27764f9b51bc8142ba3c533dfea81a5c5c9d
6ara6ac <kirill.v.abc@gmail.com>
2023-04-25 20:03:04 +0300

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,13 +8,15 @@ const UsersPage = lazy(() => import("../pages/UserPage/UsersPage"))
 
 
 export const App = () => {
-return <Routes>
-    <Route path="/" element={<Layout/>}>
+return (
+    <Routes>
+        <Route path="/" element={<Layout/>}>
         <Route index element={<HomePage />} />
         <Route path="/tweets" element={<UsersPage/>}/>
         <Route path="*" element={<HomePage />} />
-    </Route>
+        </Route>
     </Routes>
+)
 
 
 }
