636ecb7a10d106b20bc79d7c818b48660aefea7c
6ara6ac <kirill.v.abc@gmail.com>
2023-04-25 09:08:31 +0300

    upd

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-        <BrowserRouter basename="/test_work/">
+        <BrowserRouter basename="/test_task/">
                 <App />
         </BrowserRouter>
 
